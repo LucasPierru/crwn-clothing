56e1c89e6e7b12586c9ee2e20569db6c20d68b02
LucasPierru <lucaspierru7@gmail.com>
2022-08-19 22:29:47 -0400

    user and category done

--- src/index.js
+++ src/index.js
@@ -4,23 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider } from './context/user.context';
 import { CartProvider } from './context/cart.context';
-import { CategoriesProvider } from './context/categories.context';
-
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
+    <Provider store={store}>
+      <BrowserRouter>
           <CartProvider>
             <App />
           </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>  
+    </Provider>
   </React.StrictMode>
 );
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,18 +1,20 @@
 import { Fragment, useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import { UserContext } from "../../context/user.context";
+
 import { CartContext } from "../../context/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles.jsx'
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser)
   const { isCartOpen } = useContext(CartContext);
   
   const signOutHandler = async() => {
