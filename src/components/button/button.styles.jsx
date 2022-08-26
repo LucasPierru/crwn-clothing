82c572a89e45264e5821b4fcc42131c838b34a84
LucasPierru <lucaspierru7@gmail.com>
2022-08-26 00:46:17 -0400

    changed to open sans

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -13,7 +13,6 @@ export const BaseButton = styled.button`
   background-color: black; 
   color: white; 
   text-transform: uppercase; 
-  font-family: 'Open Sans Condensed', 'Open Sans'; 
   font-weight: bolder; 
   border: none; 
   cursor: pointer; 
